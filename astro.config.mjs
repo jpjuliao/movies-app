f1e08e5c78302acfd2fb53b422c77c579db308b9
Juan Pablo Juliao <jpjuliao@gmail.com>
2023-01-21 19:50:20 -0500

    Add adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind()],
+  adapter: cloudflare()
 });
